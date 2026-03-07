9b53b9f1ad8bbf4c48cfd4c83b4fab4c6a202cee
Amira Zarglayoun <zarglayoun.a@gmail.com>
2026-03-07 19:17:11 +0100

    chore: remove dead code (nav-header, unused types)

--- src/interfaces/post-model.ts
+++ src/interfaces/post-model.ts
@@ -2,8 +2,4 @@ import type { CollectionEntry } from 'astro:content'
 
 export type PostModel = CollectionEntry<'posts'>
 
-export type PostMatter = CollectionEntry<'posts'>['data']
-
 export type EssayModel = CollectionEntry<'essays'>
-
-export type EssayMatter = CollectionEntry<'essays'>['data']
